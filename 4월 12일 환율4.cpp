49eecd8d2a6b1f66b8c5dda402656790fef39faa
MINSEOK22 <102019280+MINSEOK22@users.noreply.github.com>
2022-04-12 15:08:17 +0900

    Add files via upload

---
+++ 4월 12일 환율4.cpp
@@ -0,0 +1,73 @@
+#include <stdio.h>
+	const float RATE_USD = 1233.1;
+	const float RATE_JPY = 987.39;
+	const float RATE_EUR = 1341.86;
+	const float RATE_CNY = 193.24;
+	const float RATE_GBP = 1604.76; 
+	
+	int inputWon = 0;
+	int returnWon = 0; 
+	int outputFinal = 0;				
+	float output = 0;
+	 
+	
+int exchange(float a) {
+	output = inputWon / a;
+	outputFinal = output;
+	returnWon = output * 100;
+	returnWon = returnWon % 100;
+	returnWon = returnWon * a / 100; 
+	returnWon = returnWon / 10 * 10;
+}
+
+int main() { 
+    int c500, c100, c50, c10;	
+	int currencyNUM = 0;
+	char *bill;
+	printf("환전을 원하는 원화 금액을 입력하세요 :");
+	scanf("%d", &inputWon);
+	do { 
+	printf("환전을 원하는 나라를 선택하시오 (1:USD, 2:JPY, 3: EURO, 4: CNY, 5:GBP) :");
+		scanf("%d", &currencyNUM);
+	} while (currencyNUM < 1 || currencyNUM > 5 );
+    
+	if (currencyNUM == 1) {
+			// 원화 -> USD 
+		exchange(RATE_USD);
+		bill = "달 러"; 
+	} else if (currencyNUM == 2) {
+			// 원화 -> JPY 
+		exchange(RATE_JPY);
+		bill = "엔 화";
+	} else if (currencyNUM == 3) {
+	// 원화 -> EUR	
+		exchange(RATE_EUR);
+		bill = "유 로";
+	} else if (currencyNUM == 4) {
+		// 원화 -> CNY
+		exchange(RATE_CNY);
+		bill = "위 안";
+	} else if (currencyNUM == 5) {
+	// 원화 -> GBP	
+		exchange(RATE_GBP);
+		bill = "파 운 드"; 
+	}
+	printf("%d원 -> %d %s, 받을 동전%d원\n\n", inputWon, outputFinal, bill, returnWon);
+
+	c500 = returnWon/500; 
+    returnWon = returnWon%500; 
+
+    c100 = returnWon/100; 
+    returnWon %= 100;     
+
+    c50 = returnWon/50;    
+    returnWon %= 50;       
+
+    c10 = returnWon/10;    
+    returnWon %= 10;
+
+	printf("500원 : %d개, 100원 : %d개, 50원 : %d개, 10원 : %d개\n\n", c500, c100, c50, c10);
+	
+	return 0; 
+
+}
